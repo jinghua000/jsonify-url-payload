fe2d2890f26556d8f3abd5aa8437ead27a70ffa2
shadow <jing_hua_000@qq.com>
2019-12-19 23:12:08 +0800

    update

--- src/core.ts
+++ src/core.ts
@@ -1,13 +1,56 @@
-export function mixer (
-  url: string, 
-  params: object
-): string {
+import { 
+  nativeEncode, 
+  natvieDecode, 
+  safeParse, 
+  Q_MARK, 
+  A_MARK, 
+  E_MARK 
+} from './shared'
 
-  return ''
+import { 
+  isEmpty, 
+  pipe, 
+  split, 
+  apply, 
+  merge 
+} from 'shadow-fns'
+
+export function mixer (url: string, params: object): string {
+  const arr: string[] = []
+  arr.push(url)
+
+  if (!isEmpty(params)) {
+    arr.push(Q_MARK)
+    arr.push(encode(params))
+  }
+
+  return arr.join('')
 }
 
-export function parser (
-  url: string
-): object {
-  return {}
+export function parser (url: string): object {
+  const str = url.split(Q_MARK)[1] || ''
+
+  return decode(str)
+}
+
+function encode (params: object): string {
+  return Object.keys(params).map(
+    key => [
+      nativeEncode(key), 
+      E_MARK, 
+      nativeEncode(JSON.stringify(params[key]))
+    ].join('')
+  ).join(A_MARK)
+}
+
+function decode (str: string): object {
+  if (!str) return {}
+
+  const handleArr = apply(merge)
+  const handleElem = pipe(split(E_MARK), (arr: string[]) => ({
+    [natvieDecode(arr[0])]: safeParse(natvieDecode(arr[1]))
+  }))
+  const paramsArr = str.split(A_MARK).map(handleElem)
+
+  return handleArr(paramsArr)
 }
--- test/parser.spec.ts
+++ test/parser.spec.ts
@@ -1,10 +1,56 @@
-import { eq } from './shared'
+import { equals } from './shared'
+import { mixer, parser } from '../src'
 
-describe('demo', () => {
+describe('test parser', () => {
 
-  it('demo', () => {
+  it('parser should parser the jsonify payload of url', () => {
     
-    eq(1, 1)
+    const params = { a: 123, b: '我', c: [null], d: { e: true } }
+    const url = mixer('127.0.0.1', params)
+
+    equals(parser(url), params) 
+
+  })
+
+  it('parser can parse some special symbol', () => {
+
+    const params = { a: '!@#$%^&*(){}<>[]?|";:\\\'' }
+    const url = mixer('127.0.0.1', params)
+
+    equals(parser(url), params) 
+
+  })
+
+  it('parser can parse the key of object', () => {
+
+    const params = { '我': '我' } 
+    const url = mixer('127.0.0.1', params)
+
+    equals(parser(url), params) 
+
+  })
+
+  it('parser should consider the unavailable values as undefined', () => {
+
+    const url = '127.0.0.1?aa=aa'
+
+    equals(parser(url), { aa: undefined })
+
+  })
+
+  it('parser should return empty object if no payload supplied', () => {
+
+    const url = '127.0.0.1'
+
+    equals(parser(url), {})
+
+  })
+
+  it('parser can parse the payload which is available even no encode', () => {
+
+    const url = '127.0.0.1?a="我"&b={"a":123}'
+
+    equals(parser(url), { a: '我', b: { a: 123 } })
 
   })
 
